b359d6bf107e038f685845b1641aeb51ec37a0b4
fh-kazemi84 <fh.kazemi84@google.com>
2025-08-31 01:52:21 +0200

    Create Employee Controller.

---
+++ API/EmployeeActivities/src/main/java/com/kazemi/EmployeeActivities/Controller/EmployeeController.java
@@ -0,0 +1,51 @@
+package com.kazemi.EmployeeActivities.Controller;
+
+import com.kazemi.EmployeeActivities.Models.Employee;
+import com.kazemi.EmployeeActivities.Service.EmployeeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * @author fh.kazemi
+ **/
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping
+    public List<Employee> getAll() {
+        return employeeService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Employee> getById(@PathVariable Integer id) {
+        return employeeService.getById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeService.save(employee);
+    }
+
+    @PutMapping("/{id}")
+    public Employee updateEmployee(@PathVariable Integer id, @RequestBody Employee updatedEmployee) {
+        return employeeService.update(id, updatedEmployee);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteEmployee(@PathVariable Integer id) {
+        employeeService.delete(id);
+    }
+}
