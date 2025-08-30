5b15e745d756fe9ed4ac37806a8c24159dae9d3f
fh-kazemi84 <fh.kazemi84@google.com>
2025-08-31 01:50:48 +0200

    Create Employee Repository.

---
+++ API/EmployeeActivities/src/main/java/com/kazemi/EmployeeActivities/Repository/EmployeeRepository.java
@@ -0,0 +1,16 @@
+package com.kazemi.EmployeeActivities.Repository;
+
+import com.kazemi.EmployeeActivities.Models.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * @author fh.kazemi
+ **/
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+}
