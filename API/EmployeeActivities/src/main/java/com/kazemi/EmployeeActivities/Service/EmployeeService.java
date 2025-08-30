7f9033e16910eb6673f36d507fcb42136229c3a5
fh-kazemi84 <fh.kazemi84@google.com>
2025-08-31 01:51:36 +0200

    Create Employee Service.

---
+++ API/EmployeeActivities/src/main/java/com/kazemi/EmployeeActivities/Service/EmployeeService.java
@@ -0,0 +1,50 @@
+package com.kazemi.EmployeeActivities.Service;
+
+import com.kazemi.EmployeeActivities.Models.Employee;
+import com.kazemi.EmployeeActivities.Repository.EmployeeRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * @author fh.kazemi
+ **/
+@Service
+public class EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    public List<Employee> getAll() {
+        return employeeRepository.findAll();
+    }
+
+    public Optional<Employee> getById(Integer id) {
+        return employeeRepository.findById(id);
+    }
+
+    public Employee save(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public void delete(Integer id) {
+        employeeRepository.findById(id)
+                .ifPresentOrElse(
+                        employeeRepository::delete,
+                        () -> { throw new RuntimeException("Employee not found with id: " + id); }
+                );
+    }
+
+    public Employee update(Integer id, Employee updatedEmployee) {
+        return employeeRepository.findById(id)
+                .map(employee -> {
+                    employee.setFirstName(updatedEmployee.getFirstName());
+                    employee.setLastName(updatedEmployee.getLastName());
+                    return employeeRepository.save(employee);
+                }).orElseThrow(() -> new RuntimeException("Employee not found with id: " + id));
+    }
+}
