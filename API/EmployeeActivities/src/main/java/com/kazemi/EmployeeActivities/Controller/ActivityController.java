24ccdef9382c19f3d2b9d62487839f984316089d
fh-kazemi84 <fh.kazemi84@google.com>
2025-09-13 22:33:15 +0200

    add: importFromExcel method to Controller class.

--- API/EmployeeActivities/src/main/java/com/kazemi/EmployeeActivities/Controller/ActivityController.java
+++ API/EmployeeActivities/src/main/java/com/kazemi/EmployeeActivities/Controller/ActivityController.java
@@ -2,10 +2,8 @@ package com.kazemi.EmployeeActivities.Controller;
 
 import com.kazemi.EmployeeActivities.Models.Activity;
 import com.kazemi.EmployeeActivities.Service.ActivityService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author fh.kazemi
@@ -24,5 +22,12 @@ public class ActivityController {
                              @RequestParam String sheet,
                              @RequestParam String cell) throws Exception {
         return activityService.getActivity(filePath, sheet, cell);
+    }
+
+    @PostMapping("/import")
+    public ResponseEntity<String> importFromExcel(@RequestParam String filePath,
+                                @RequestParam String sheet) throws Exception {
+        activityService.importFromExcel(filePath, sheet);
+        return ResponseEntity.ok("Import successful!");
     }
 }
