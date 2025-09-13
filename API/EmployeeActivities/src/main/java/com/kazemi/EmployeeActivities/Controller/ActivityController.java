a989ba238b52be3921be18d4baf2e0a4c87dbf6e
fh-kazemi84 <fh.kazemi84@gmail.com>
2025-09-14 00:06:55 +0200

    refactor: rename package name.

--- API/EmployeeActivities/src/main/java/com/kazemi/EmployeeActivities/Controller/ActivityController.java
+++ API/EmployeeActivities/src/main/java/com/kazemi/EmployeeActivities/Controller/ActivityController.java
@@ -1,6 +1,6 @@
 package com.kazemi.EmployeeActivities.Controller;
 
-import com.kazemi.EmployeeActivities.Models.Activity;
+import com.kazemi.EmployeeActivities.Model.Activity;
 import com.kazemi.EmployeeActivities.Service.ActivityService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
