d47523b808c98139d3fb698e4ae5ad017a2a88ed
fh-kazemi84 <fh.kazemi84@gmail.com>
2025-09-14 01:17:00 +0200

    refactor: integrate EmployeeMapper in EmployeeService and update methods to use DTOs.

--- API/EmployeeActivities/src/main/java/com/kazemi/EmployeeActivities/Service/EmployeeService.java
+++ API/EmployeeActivities/src/main/java/com/kazemi/EmployeeActivities/Service/EmployeeService.java
@@ -1,11 +1,12 @@
 package com.kazemi.EmployeeActivities.Service;
 
+import com.kazemi.EmployeeActivities.DTO.EmployeeDTO;
+import com.kazemi.EmployeeActivities.Mapper.EmployeeMapper;
 import com.kazemi.EmployeeActivities.Model.Employee;
 import com.kazemi.EmployeeActivities.Repository.EmployeeRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 /**
  * @author fh.kazemi
@@ -14,25 +15,46 @@ import java.util.Optional;
 public class EmployeeService {
 
     private final EmployeeRepository employeeRepository;
+    private final EmployeeMapper mapper;
 
-    public EmployeeService(EmployeeRepository employeeRepository) {
+    public EmployeeService(EmployeeRepository employeeRepository,
+                           EmployeeMapper employeeMapper) {
         this.employeeRepository = employeeRepository;
+        this.mapper = employeeMapper;
     }
 
-    public List<Employee> getAll() {
-        return employeeRepository.findAll();
+    public List<EmployeeDTO> getAll() {
+        List<Employee> employeeList = employeeRepository.findAll();
+        return mapper.toDtoList(employeeList);
     }
 
-    public Optional<Employee> getById(Integer id) {
-        return employeeRepository.findById(id);
+    public EmployeeDTO getById(Integer id) {
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Employee not found with id: " + id));
+        return mapper.toDto(employee);
     }
 
-    public List<Employee> getByName(String name){
-        return employeeRepository.findByName(name);
+    public List<EmployeeDTO> getByName(String name){
+        List<Employee> employeeList = employeeRepository.findByName(name);
+        return mapper.toDtoList(employeeList);
     }
 
-    public Employee save(Employee employee) {
-        return employeeRepository.save(employee);
+    public EmployeeDTO save(EmployeeDTO employeeDto) {
+        Employee employee = mapper.toEntity(employeeDto);
+        Employee savedEmployee = employeeRepository.save(employee);
+        return mapper.toDto(savedEmployee);
+    }
+
+    public EmployeeDTO update(Integer id, EmployeeDTO employeeDTO) {
+        Employee employee = mapper.toEntity(employeeDTO);
+        Employee updatedEmployee = employeeRepository.findById(id)
+                .map(e -> {
+                    e.setFirstName(employee.getFirstName());
+                    e.setLastName(employee.getLastName());
+                    e.setActivityList(employee.getActivityList());
+                    return employeeRepository.save(e);
+                }).orElseThrow(() -> new RuntimeException("Employee not found with id: " + id));
+        return mapper.toDto(updatedEmployee);
     }
 
     public void delete(Integer id) {
@@ -41,14 +63,5 @@ public class EmployeeService {
                         employeeRepository::delete,
                         () -> { throw new RuntimeException("Employee not found with id: " + id); }
                 );
-    }
-
-    public Employee update(Integer id, Employee updatedEmployee) {
-        return employeeRepository.findById(id)
-                .map(employee -> {
-                    employee.setFirstName(updatedEmployee.getFirstName());
-                    employee.setLastName(updatedEmployee.getLastName());
-                    return employeeRepository.save(employee);
-                }).orElseThrow(() -> new RuntimeException("Employee not found with id: " + id));
     }
 }
