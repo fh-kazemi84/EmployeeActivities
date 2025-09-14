66c8efaa7a8146fc978158c46293a231dca555fb
fh-kazemi84 <fh.kazemi84@gmail.com>
2025-09-14 21:47:40 +0200

    refactor: integrate JpaSpecificationExecutor and EmployeeSpecifications in EmployeeRepository.

--- API/EmployeeActivities/src/main/java/com/kazemi/EmployeeActivities/Repository/EmployeeRepository.java
+++ API/EmployeeActivities/src/main/java/com/kazemi/EmployeeActivities/Repository/EmployeeRepository.java
@@ -1,7 +1,9 @@
 package com.kazemi.EmployeeActivities.Repository;
 
 import com.kazemi.EmployeeActivities.Model.Employee;
+import com.kazemi.EmployeeActivities.Specification.EmployeeSpecifications;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -12,16 +14,13 @@ import java.util.List;
  * @author fh.kazemi
  **/
 @Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+public interface EmployeeRepository extends
+        JpaRepository<Employee, Integer>,
+        JpaSpecificationExecutor<Employee> {
 
-    @Query("SELECT e FROM Employee e " +
-            "WHERE LOWER(e.firstName) LIKE LOWER(CONCAT('%', :name, '%')) " +
-            "OR LOWER(e.lastName) LIKE LOWER(CONCAT('%', :name, '%')) " +
-            "OR LOWER(CONCAT(e.firstName, ' ', e.lastName)) LIKE LOWER(CONCAT('%', :name, '%')) " +
-            "OR LOWER(CONCAT(e.firstName, e.lastName)) LIKE LOWER(CONCAT('%', :name, '%')) " +
-            "OR LOWER(CONCAT(e.lastName, ' ', e.firstName)) LIKE LOWER(CONCAT('%', :name, '%')) " +
-            "OR LOWER(CONCAT(e.lastName, e.firstName)) LIKE LOWER(CONCAT('%', :name, '%'))")
-    List<Employee> findByName(@Param("name") String name);
+    default List<Employee> findByName(String name) {
+        return findAll(EmployeeSpecifications.hasNameLike(name));
+    }
 
     List<Employee> findByFirstNameIgnoreCaseAndLastNameIgnoreCase(String firstName, String lastName);
 }
